6496e8a193899e23e2ae629c626290dfe8d1de3b
Attila Kocis <attila@dl1nux.de>
2022-08-13 19:53:09 +0200

    LoRa SDR RX

--- stop.sh
+++ stop.sh
@@ -1,2 +1,2 @@
 # Hiermit beenden wir alle potenziell laufenden Prozesse
-sudo killall -9 rtl_tcp sdrtst afskmodem udpbox udpflex udpgate4 ra02
+sudo killall -9 rtl_tcp sdrtst afskmodem udpbox udpflex udpgate4 ra02 lorarx
